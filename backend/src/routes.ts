83c3da240f9921ca5d9b28d58c6daca6b473af09
Fellipe Castro <fellipecastro27d@gmail.com>
2024-05-07 17:39:58 -0300

    create não esta funcionando

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify"
+import { CreateCustomerController } from "./controllers/CreateCustomerController"
 
 export const routes = async (fastify: FastifyInstance, options: FastifyPluginOptions) => {
     fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
         return { msg: "Hello World" }
+    })
+
+    fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply)
     })
 }
